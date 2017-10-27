79c15df294ba42812854d98df44c16e0343bab3a
Antoine Bergerault <antoine.bergerault@gmail.com>
2017-10-27 22:45:50 +0200

    Better performance

--- song.py
+++ song.py
@@ -1,23 +1,21 @@
 import string #pour les notes
 import random #pour les randoms
 import time #pour le temps
+from math import ceil #pour l'arrondi superieur
+from math import floor #pour l'arrondi inferieur
 debut = time.time() #debut temps
 liste = []
 #nbr = 1 #degre d'evolution
-timing = 760000 #90000 21 sec pour telecharger
-aigu = 80 #le plus aigu
+timing = 790000 #90000 est bien pour les tests
+aigu = 40 #le plus aigu
 grave = 200 #le plus grave
-tempo = 74 #Plus le tempo est grand plus il y aura de rythme
+tempo = 60 #Plus le tempo est grand plus il y aura de rythme
 flow = (aigu + grave)/2 * (aigu + grave)/tempo #calcul longueur note
 loop = timing/flow #calcul notes avec le temps et la longueur note
-d = flow/2 #decalage de la longeur
+d = flow/1.5 #decalage de la longeur
 mini = flow - d #longueur minimum
 maxi = flow + d #longueur maximum
 fichier = open("Song.txt", "w")
-def Random_str(cnt):
-    return random.choice(cnt)
-def RndIn(a,b):
-    return random.randint(a,b)
 def song(mini,maxi,l):
     #ajuste maximum avec grave
     b = (l/grave) * (maxi - mini)
@@ -25,16 +23,15 @@ def song(mini,maxi,l):
     return random.randint(mini,big)
 s = string.letters
 liste = s
-dataevent = loop/180
+dataevent = int(ceil(timing/100000)) #calcul pour frequence affichage pourcentage
 for i in range(loop):
-    r = Random_str(liste)
+    r = random.choice(liste)
 #    for u in range(nbr):
 #        liste.append(r)
-    b = RndIn(aigu,grave)
+    b = random.randint(aigu,grave)
     for y in range(song(mini,maxi,b)):
         fichier.write(r)
-        for t in range(b):
-            fichier.write(' ')
+        fichier.write(' ' * b)
     if(i%(loop/dataevent) == 0):
         print ''.join([repr((i*100)/loop),'%'])
 fichier.close()
