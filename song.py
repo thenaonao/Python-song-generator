4d45f73a5abb43f375c94d5e19b07f7d1a9af9d4
AqwerfNet <coucou48500@yahoo.fr>
2021-02-15 05:57:01 +0900

    Simple proof of concept
    Missing sharp notes, and multiple chords

--- song.py
+++ song.py
@@ -1,59 +1,147 @@
-import string #pour les notes
-import random #pour les randoms
-import time #pour le temps
-from math import ceil #pour l'arrondi superieur
-from math import floor #pour l'arrondi inferieur
-debut = time.time() #debut temps
-liste = []
-#nbr = 1 #degre d'evolution
-timing = 90000 #90000 est bien pour les tests
-aigu = 50 #le plus aigu
-grave = 400 #le plus grave
-tempo = 92 #Plus le tempo est grand plus il y aura de rythme 82 max
-flow = int(ceil((aigu + grave)/2 * (aigu + grave)/tempo)) #calcul longueur note
-loop = int(ceil(timing/flow)) #calcul notes avec le temps et la longueur note
-d = flow/1.5 #decalage de la longeur
-mini = flow - d #longueur minimum
-maxi = flow + (d*6) #longueur maximum
-change = 75 #pourcentage de changement
-percent_sensibility = 1.5 #1.5 est bien
-fichier = open("Song.txt", "w")
-def song(mini,maxi,l):
-    #ajuste maximum avec grave
-    b = (l/grave) * (maxi - mini)
-    big = maxi - b
-    mini = int(mini)
-    big = int(big)
-    return random.randint(mini,big)
-def modify(a,d):
-    z = (a*0.9)/d * (change/100)
-    mini_v = (aigu - b)
-    mini_v = int(floor(mini_v * z))
-    maxi_v = grave - b
-    maxi_v = floor(maxi_v * z)
-    v = random.randint(mini_v,maxi_v)
-    p = v/a
-    return p
-s = string.letters
-liste = s
-time_percent = 100000 * percent_sensibility
-dataevent = int(ceil(timing/time_percent)) #calcul pour frequence affichage pourcentage
-for i in range(loop):
-    r = random.choice(liste)
-#    for u in range(nbr):
-#        liste.append(r)
-    b = random.randint(aigu,grave)
-    len_song = song(mini,maxi,b)
-    v = 0
-    p = 0
-    if(random.randint(0,100) > 50):
-        p = modify(len_song,maxi)
-    for y in range(len_song):
-        b = b + p
-        fichier.write(r)
-        fichier.write(' ' * b)
-    if(i%(loop/dataevent) == 0):
-        print ''.join([repr((i*100)/loop),'%'])
-fichier.close()
-fin = time.time()
-print fin - debut
+import string 
+from random import randint
+import time 
+from math import ceil 
+from math import floor
+from math import sin
+from math import cos
+from math import sqrt
+
+do = 32.70320 
+re = 36.70810  
+mi = 41.20344 
+fa = 43.65353 
+sol = 48.99943
+la = 55.00000 
+si = 61.73541 
+
+PI = 3.141592
+
+def abs(a):
+    if a<0:
+        return -a
+    return a
+
+def note_do(length,octave):
+    do_array=[]
+    for i in range(length):
+        note = int( sin(2*PI*do*pow(2,octave)*i/44100)*127*((length*3-i)/(length*3))+127)
+        do_array.append(note)
+    return do_array
+
+def note_re(length,octave):
+    re_array=[]
+    for i in range(length):
+        note = int( sin(i*2*PI*re*pow(2,octave)/44100)*127*((length*3-i)/(length*3))+127)
+        re_array.append(note)
+    return re_array
+
+def note_mi(length,octave):
+    mi_array=[]
+    for i in range(length):
+        note = int( sin(i*2*PI*mi*pow(2,octave)/44100)*127*((length*3-i)/(length*3))+127)
+        mi_array.append(note)
+    return mi_array
+
+def note_fa(length,octave):
+    fa_array=[]
+    for i in range(length):
+        note = int( sin(i*2*PI*fa*pow(2,octave)/44100)*127*((length*3-i)/(length*3))+127)
+        fa_array.append(note)
+    return fa_array
+
+def note_sol(length,octave):
+    sol_array=[]
+    for i in range(length):
+        note = int( sin(i*2*PI*sol*pow(2,octave)/44100)*127*((length*3-i)/(length*3))+127)
+        sol_array.append(note)
+    return sol_array
+
+def note_la(length,octave):
+    la_array=[]
+    for i in range(length):
+        note = int( sin(i*2*PI*la*pow(2,octave)/44100)*127*((length*3-i)/(length*3))+127)
+        la_array.append(note)
+    return la_array
+
+def note_si(length,octave):
+    si_array=[]
+    for i in range(length):
+        note = int( sin(i*2*PI*si*pow(2,octave)/44100)*127*((length*3-i)/(length*3))+127)
+        si_array.append(note)
+    return si_array
+
+#44100 = 1 Sec; 44.1 = 1 ms
+def chute(time):
+    chute_array=[]
+    for k in range(0,time):
+        chute_array.append(127)
+    return chute_array
+
+def choose_note():
+    r=randint(0,6)
+    if r==0:
+        return la
+    elif r==1:
+        return si
+    elif r==2:
+        return do
+    elif r==3:
+        return re
+    elif r==4:
+        return mi
+    elif r==5:
+        return fa
+    else:
+        return sol
+
+#generate one note
+def gen_note(length, freq):
+    note_array=[]
+    octave=randint(2,4)
+    for i in range(0,length):
+        note = int( sin(i*2*PI*freq*pow(2,octave)/44100)*127*((length*3-i)/(length*3))+127)
+        #note = int( sin(i*2*PI*freq*pow(2,octave)/44100)*127*(1/sqrt(i+1))+127) #New way of attenuation, very primitive. . .
+        note_array.append(note)
+    print(length/44100)
+    return note_array
+
+#generate the whole music
+def gen_notes_track(howmany):
+    notes_track_array=[]
+    #Hard coded "Au Clair de la Lune"
+    notes_track_array+=note_do(2400*11,4)
+    notes_track_array+=chute(20)
+    notes_track_array+=note_do(2400*11,4)
+    notes_track_array+=chute(20)
+    notes_track_array+=note_do(2400*11,4)
+    notes_track_array+=chute(20)
+    notes_track_array+=note_re(2400*11,4)
+    notes_track_array+=chute(20)
+    notes_track_array+=note_mi(4800*11,4)
+    notes_track_array+=chute(20)
+    notes_track_array+=note_re(4800*11,4)
+    notes_track_array+=chute(20)
+    notes_track_array+=note_do(2400*11,4)
+    notes_track_array+=chute(20)
+    notes_track_array+=note_mi(2400*11,4)
+    notes_track_array+=chute(20)
+    notes_track_array+=note_re(2400*11,4)
+    notes_track_array+=chute(20)
+    notes_track_array+=note_re(2400*11,4)
+    notes_track_array+=chute(20)
+    notes_track_array+=note_do(7200*11,4)
+    notes_track_array+=chute(20)
+    
+    
+    #for i in range(howmany):
+    #    notes_track_array+=gen_note(randint(440,44100),choose_note())
+    return notes_track_array
+
+
+f = open('song.aqraw', 'wb')
+note_array=bytearray(gen_notes_track(randint(30,120)))
+f.write(note_array)
+
+f.close()
+print("Song generated!")
